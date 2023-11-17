d640369774480c225c3a954db5802e8465981be3
pkattihexa <pkatti@hexalytics.com>
2023-11-17 08:56:10 +0530

    added sample file

---
+++ hexalyticsdemo/src/test/java/com/hexalyticsdemo/hexalyticsdemo/SampleTest.java
@@ -0,0 +1,10 @@
+package com.hexalyticsdemo.hexalyticsdemo;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git repo push/pull");
+
+	}
+
+}
